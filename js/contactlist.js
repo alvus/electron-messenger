643668d897259eefd20bb1b68480d32794fa76fc
zaval <dzavalny@gmail.com>
2015-10-19 00:55:44 +0300

    contact list auto update

--- js/contactlist.js
+++ js/contactlist.js
@@ -12,19 +12,23 @@ var vk = new VK({
 	'language': 'ru'
 });
 
+
 vk.setToken(cnf.token);
 vk.setSecureRequests(true);
 loadAccountInfo();
 loadContactList();
+
+setInterval(loadContactList, 10000);
 
 function loadAccountInfo(){
 	vk.request('users.get', {fields: 'photo_50'}, function(data){
 		if (!data.response){
 			return;
 		}
-		console.log(JSON.stringify(data));
+		// console.log(JSON.stringify(data));
 		$('#acc-name').text(data.response[0].first_name + ' ' + data.response[0].last_name);
 		$('#my-profile-image').attr('src', data.response[0].photo_50);
+
 	});
 }
 
@@ -35,17 +39,26 @@ function loadContactList(){
 		};
 
 		var userArray = data.response.items;
+		var contactsHtml = '';
 		
 		for(var i = 0; i < userArray.length; i++){
 			if (!userArray[i].online){
 				continue;
 			};
-			var divString = '<div class="contact"  onclick="loadChat(' + userArray[i].id + ')" ><div class="avatar"><img src="' + userArray[i].photo_50 + '" height="25" width="25"></div><div class="name" id="contact_' +  userArray[i].id+ '"><b>' + userArray[i].first_name + ' ' + userArray[i].last_name + '</b></div></div>';
+			// var divString = '<div class="contact"  onclick="loadChat(' + userArray[i].id + ')" ><div class="avatar"><img src="' + userArray[i].photo_50 + '" height="25" width="25"></div><div class="name" id="contact_' +  userArray[i].id+ '"><b>' + userArray[i].first_name + ' ' + userArray[i].last_name + '</b></div></div>';
+			contactsHtml += '<div class="contact"  onclick="loadChat(' + userArray[i].id + ')" ><div class="avatar"><img src="' + userArray[i].photo_50 + '" height="25" width="25"></div><div class="name" id="contact_' +  userArray[i].id+ '"><b>' + userArray[i].first_name + ' ' + userArray[i].last_name + '</b></div></div>';
 			
-			var contactDiv = $(divString);
+			// var contactDiv = $(divString);
 
-			$('main').append(contactDiv);
+			// $('main').append(contactDiv);
 		}
+		var loadingDiv = document.querySelector('#loading-div') || null;
+		if (loadingDiv){
+			loadingDiv.remove();
+		}
+		document.querySelector('main').innerHTML = '';
+		// alert('updating');
+		$('main').append($(contactsHtml));
 	});
 }
 
